994ed2af7ee91580dfdc307d61d48b0f6716a340
kendovivi <kendovivi@gmail.com>
2020-01-22 16:13:17 +0800

    draft course layout

--- app/src/main/java/com/courses/view/CourseListActivity.kt
+++ app/src/main/java/com/courses/view/CourseListActivity.kt
@@ -1,12 +1,13 @@
-package com.courses
+package com.courses.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.courses.R
 
-class MainActivity : AppCompatActivity() {
+class CourseListActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_course_list)
     }
 }
